409b7686a24b45a2a5110e2d874f5f49d0e259d5
sairam02 <dssairamy@gmail.com>
2024-04-16 17:16:56 +0530

    data ingestion done in notebook
    Signed-off-by: sairam02 <dssairamy@gmail.com>

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -8,7 +8,6 @@ from pathlib import Path
 from typing import Any
 
 
-
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
@@ -26,7 +25,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
-            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            logger.info(f"yaml file:{path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
